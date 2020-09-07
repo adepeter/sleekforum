4b14c1d3e2713b1abf3a04f4e6b0670070a1428d
Oluwaseun Peter <adepeter26@gmail.com>
2020-09-07 18:09:32 +0100

    Add ability to distinguish days of thread creation dates

--- src/sleekapps/cores/helper.py
+++ src/sleekapps/cores/helper.py
@@ -1,9 +1,18 @@
 from django.conf import settings
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.templatetags.static import static
+from django.utils.timezone import now
+
+date_now = now()
+
 
 def get_static(path_to_file_or_directory):
     if settings.DEBUG:
         return staticfiles_storage.url(path_to_file_or_directory)
     else:
         return static(path_to_file_or_directory)
+
+
+def calculate_days_interval(date_obj):
+    diff = date_now - date_obj
+    return diff.days
--- src/sleekapps/cores/templatetags/cores_util_tags.py
+++ src/sleekapps/cores/templatetags/cores_util_tags.py
@@ -5,6 +5,8 @@ from django.template.defaultfilters import stringfilter
 from django.utils import timezone
 from django.utils.translation import ngettext, gettext_lazy as _
 
+from ..helper import calculate_days_interval
+
 register = template.Library()
 
 
@@ -24,39 +26,39 @@ def time_ago(param):
     elif diff.days == 0 and 60 <= diff.seconds < 3600:
         minutes = math.floor(diff.seconds / 60)
         currently = ngettext(_('%(minutes)d minute ago'),
-                          _('%(minutes)d minutes ago'), minutes) % {
-                     'minutes': minutes
-                 }
+                             _('%(minutes)d minutes ago'), minutes) % {
+                        'minutes': minutes
+                    }
     elif diff.days == 0 and 3600 <= diff.seconds < 86400:
         hours = math.floor(diff.seconds / 3600)
         currently = ngettext(_('%(hours)d hour ago'),
-                          _('%(hours)d hours ago'), hours) % {
-                     'hours': hours
-                 }
+                             _('%(hours)d hours ago'), hours) % {
+                        'hours': hours
+                    }
     elif 1 <= diff.days <= 7:
         days = diff.days
         currently = ngettext(_('%(days)d day ago'),
-                          _('%(days)d days ago'), days) % {
-                     'days': days
-                 }
+                             _('%(days)d days ago'), days) % {
+                        'days': days
+                    }
     elif 7 <= diff.days <= 30:
         weeks = math.floor(diff.days / 7)
         currently = ngettext(_('%(weeks)d week ago'),
-                          _('%(weeks)d weeks ago'), weeks) % {
-                     'weeks': weeks
-                 }
+                             _('%(weeks)d weeks ago'), weeks) % {
+                        'weeks': weeks
+                    }
     elif 30 <= diff.days <= 365:
         months = math.floor(diff.days / 30)
         currently = ngettext(_('%(months)d month ago'),
-                          _('%(months)d months ago'), months) % {
-                     'months': months
-                 }
+                             _('%(months)d months ago'), months) % {
+                        'months': months
+                    }
     elif diff.days >= 365:
         years = math.floor(diff.days / 365)
         currently = ngettext(_('%(years)d year ago'),
-                          _('%(years)d years ago'), years) % {
-                     'years': years
-                 }
+                             _('%(years)d years ago'), years) % {
+                        'years': years
+                    }
     return currently
 
 
@@ -90,3 +92,8 @@ def get_dictionary_value(result, key):
         return result[key]
     except KeyError:
         return None
+
+
+@register.simple_tag(name='days_past_interval')
+def interval_calculator(date_obj):
+    return calculate_days_interval(date_obj)
