c5812caa9cbbbf29195f46678dc212a663aa2471
Oluwaseun Peter <adepeter26@gmail.com>
2020-09-09 00:15:07 +0100

    Add pages navigation to mobile view

--- src/sleekapps/pages/urls.py
+++ src/sleekapps/pages/urls.py
@@ -10,7 +10,9 @@ urlpatterns = [
 ]
 
 urlpatterns += [
+    path('faq/', views.flatpage, {'url': '/faq/'}, name='faq'),
     path('about-us/', views.flatpage, {'url': '/about-us/'}, name='about_us'),
+    path('disclaimer/', views.flatpage, {'url': '/disclaimer/'}, name='disclaimer'),
     path('privacy-policy/', views.flatpage, {'url': '/privacy-policy/'}, name='privacy_policy'),
     path('terms-and-conditions/', views.flatpage, {'url': '/terms-and-conditions/'}, name='terms'),
 ]
