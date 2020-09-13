cf886b02c209568c3ddce9f402ccd38901c5c979
Oluwaseun Peter <adepeter26@gmail.com>
2020-09-13 01:19:17 +0100

    Implementing site wide configuration settings

--- src/sleekapps/settings/admin.py
+++ src/sleekapps/settings/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import Setting
+
+
+@admin.register(Setting)
+class SettingsAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    fieldsets = [
+        (_('Basic'), {'fields': ['title', 'description']}),
+        (_('Paginations'), {'fields': ['threads_per_page', 'posts_per_thread']}),
+    ]
--- src/sleekapps/settings/models.py
+++ src/sleekapps/settings/models.py
@@ -10,6 +10,10 @@ class Setting(Configuration):
         max_length=20,
         help_text=_('website title')
     )
+    description = models.TextField(
+        verbose_name=_('site description'),
+        blank=True
+    )
     email = models.EmailField(
         verbose_name=_('e-mail'),
         default='noreply@localhost',
@@ -19,7 +23,29 @@ class Setting(Configuration):
         help_text=_('message after successful registration'),
         blank=True
     )
+    under_maintenance = models.BooleanField(
+        verbose_name=_('maintenance status'),
+        default=False,
+        help_text=_('Determine if site is under maintenance')
+    )
+    send_welcome_mail = models.BooleanField(
+        verbose_name=_('send welcome email'),
+        default=False,
+        help_text=_('Send welcome mail to new users')
+    )
     allow_registration = models.BooleanField(default=True)
-    captcha = models.BooleanField(default=False, help_text=_('Turn on/off captcha on pages'))
-    threads_per_page = models.PositiveSmallIntegerField(default=10)
-    posts_per_thread = models.PositiveSmallIntegerField(default=10)
+    captcha = models.BooleanField(
+        verbose_name=_('captcha verification'),
+        default=False,
+        help_text=_('Turn on/off captcha on pages')
+    )
+    threads_per_page = models.PositiveSmallIntegerField(
+        verbose_name=_('threads on a page'),
+        default=10,
+        help_text=_('Number of threads to show on a page')
+    )
+    posts_per_thread = models.PositiveSmallIntegerField(
+        verbose_name=_('posts per thread'),
+        default=10,
+        help_text=_('Number of posts to display per thread')
+    )
