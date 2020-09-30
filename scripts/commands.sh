dd3aa43feb8f59e5570903f050af4ee00b7c2451
Oluwaseun Peter <adepeter26@gmail.com>
2020-09-30 22:07:18 +0100

    Add validators script to verify docker-compose services

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ./validators.sh
+
 redis_flush() {
   redis-cli FLUSHALL
 }
@@ -21,7 +23,8 @@ run_migrate() {
 
 setup_postgres() {
   env_file="$(get_env_dir)/postgres.env"
-  echo -e "POSTGRES_USER=postgres\nPOSTGRES_PASSWORD=postgres\nPOSTGRES_DB=postgres" >${env_file}
+  echo -e "POSTGRES_USER=postgres\nPOSTGRES_PASSWORD=postgres" > ${env_file}
+  echo -e "POSTGRES_DB=postgres" >> ${env_file}
 }
 
 setup_sleekforum() {
@@ -35,6 +38,22 @@ stop_all_services() {
 
 start_all_services() {
   docker-compose up -d
+}
+
+start_service_by_name() {
+  test -z $1 && echo "You must supply a service name"; exit 1
+  validate_service $1
+  if [[ $? -eq 0 ]]; then
+    docker-compose start $1
+  fi
+}
+
+stop_service_by_name() {
+  test -z $1 && echo "You must supply a service name to terminate"; exit 1
+  validate_service $1
+  if [[ $? -eq 0 ]]; then
+    docker-compose stop $1
+  fi
 }
 
 setup_install() {
