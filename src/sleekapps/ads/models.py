a2633e40ff984388cb1eb807ae55a041bfe5a75b
Oluwaseun Peter <adepeter26@gmail.com>
2020-10-28 22:36:16 +0100

    Creates a model data structure for ADS

--- src/sleekapps/ads/models.py
+++ src/sleekapps/ads/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Advertisement(models.Model):
+    name = models.CharField(
+        verbose_name=_('ADS Name'),
+        max_length=50
+    )
+    link = models.URLField(
+        verbose_name=_('URL of ADS'),
+        default='https://',
+    )
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
