6ea5446d8ad2eae42d155036eec2d13cc18d24e8
Oluwaseun Peter <adepeter26@gmail.com>
2020-11-27 17:31:34 +0100

    Add undermaintainance in a try catch block

--- src/sleekapps/settings/middlewares.py
+++ src/sleekapps/settings/middlewares.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.urls import resolve, reverse
+
+from ..cores.exceptions import SiteSettingsNotConfigured
+
 from .models import Setting as Configuration
 
 
@@ -8,10 +11,13 @@ class UnderMaintenanceMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        settings = Configuration.load()
-        if settings.is_under_maintenance:
-            if not request.user.is_superuser or not self.resolve_path('admin:index'):
-                return render(request, 'under_maintenance.html')
+        try:
+            settings = Configuration.load()
+            if settings.is_under_maintenance:
+                if not request.user.is_superuser or not self.resolve_path('admin:index'):
+                    return render(request, 'under_maintenance.html')
+        except SiteSettingsNotConfigured:
+            pass
         response = self.get_response(request)
         return response
 
