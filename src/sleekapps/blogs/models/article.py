c7ef2fd6636d07fa20e9dbd6db12327789bbf1a0
Oluwaseun Peter <adepeter26@gmail.com>
2021-04-25 12:28:10 +0100

    Created models for blogs app

---
+++ src/sleekapps/blogs/models/article.py
@@ -0,0 +1,73 @@
+from django.conf import settings
+from django.db import models
+from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
+
+
+class Article(models.Model):
+    MARK_DRAFT = 0
+    MARK_COMPLETED = 1
+
+    COMPLETION_STATUS_CHOICES = (
+        (MARK_DRAFT, _('draft')),
+        (MARK_COMPLETED, _('completed'))
+    )
+
+    category = models.ForeignKey(
+        'blogs.Category',
+        verbose_name=_('category'),
+        on_delete=models.CASCADE,
+        related_name='articles'
+    )
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        related_name='blogs',
+        null=True
+    )
+    title = models.CharField(
+        verbose_name=_('Title'),
+        max_length=255,
+    )
+    slug = models.SlugField(
+        verbose_name=_('Slug'),
+        editable=False,
+        blank=True
+    )
+    content = models.TextField(
+        verbose_name='article body',
+        blank=True,
+        null=True,
+        help_text=_('If content is not entered, its marked as draft')
+    )
+    completion_status = models.IntegerField(
+        verbose_name=_('completion Status'),
+        choices=COMPLETION_STATUS_CHOICES,
+        default=MARK_DRAFT,
+        editable=False,
+        help_text=_('Choose status of blog default is DRAFT')
+    )
+    is_locked = models.BooleanField(
+        verbose_name=_('Lock'),
+        default=True
+    )
+    is_hidden = models.BooleanField(
+        verbose_name=_('Hide'),
+        default=False
+    )
+    date_created = models.DateTimeField(
+        verbose_name=_('Date created'),
+        auto_now_add=True
+    )
+    date_modified = models.DateTimeField(
+        verbose_name=_('Date modified'),
+        auto_now=True
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
