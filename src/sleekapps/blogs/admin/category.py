f3d301150111b7c584be29875851783d1824ea70
Oluwaseun Peter <adepeter26@gmail.com>
2021-04-26 17:42:28 +0100

    Upgraded version to newest stable django

--- src/sleekapps/blogs/admin/category.py
+++ src/sleekapps/blogs/admin/category.py
@@ -1,17 +1,38 @@
 from django.contrib import admin
+from django.db.models import Count
 
 from ..models import Category
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    list_display_links = [
+        'icon',
+        'name'
+    ]
     list_display = [
+        'icon',
         'name',
+        'owner',
         'slug',
         'description',
         'is_locked',
         'is_hidden'
     ]
+    list_filter = [
+        'is_hidden',
+        'is_locked'
+    ]
+
+    @admin.display(description='')
+    def get_articles(self, category):
+        return category.articles_count
+
+    def get_queryset(self, request):
+        return super().get_queryset().annotate(
+            articles_count=Count('article'),
+            comments_count=Count('comments')
+        )
 
 
 __all__ = [
