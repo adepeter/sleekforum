856c672b4abd1efa357528d7f92abaf71d613218
Oluwaseun Peter <adepeter26@gmail.com>
2021-07-24 11:40:57 +0100

    Add template for notification successfully cleared

--- src/sleekapps/notifications/views.py
+++ src/sleekapps/notifications/views.py
@@ -1,6 +1,11 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.views import View
 from django.views.generic import ListView
+from django.utils.translation import gettext_lazy as _
 
 from .models import Notification
 
@@ -18,8 +23,8 @@ class NotificationListView(LoginRequiredMixin, ListView):
 
 
 class MarkAllAsRead(LoginRequiredMixin, View):
+
     def get(self, request, *args, **kwargs):
-        Notification.objects.filter(user=request.user)
-        print(self.request.user)
-        from django.http import HttpResponse
-        return HttpResponse('<b>Hello!</b>')
+        Notification.objects.filter(user=request.user).update(is_read=True)
+        messages.success(request, _('All notifications have been marked as read'))
+        return HttpResponseRedirect(reverse('sleekforum:notifications:notifications_list'))
